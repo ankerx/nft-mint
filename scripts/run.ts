7d7541701ac939be0c506837c86397f90426f25f
ankerx <93661548+ankerx@users.noreply.github.com>
2022-11-06 22:25:22 -0400

    deploy

---
+++ scripts/run.ts
@@ -0,0 +1,26 @@
+import { ethers } from "hardhat";
+
+const main = async () => {
+  const nftContractFactory = await ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+
+  let txn = await nftContract.makeAnEpicNFT();
+  await txn.wait();
+
+  txn = await nftContract.makeAnEpicNFT();
+  await txn.wait();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
